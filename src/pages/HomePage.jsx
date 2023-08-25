86f3f25d8c77bd88d3ceac09c28f75c689310043
Oscar Mesejo <oscar.mesejo@gmail.com>
2023-08-25 16:43:53 +0200

    iteration 3 done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,19 @@
+import { useState, useEffect } from "react"; 
+
 function HomePage() {
+
+    const [countries, setCountries] = useState([]);
+    const apiUrl = "https://ih-countries-api.herokuapp.com/countries";
+
+    useEffect(() => {            
+        fetch(apiUrl)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => setCountries(data))
+        .catch((err) => console.log(err))
+        
+      }, [] );
 
     return (
 
@@ -6,36 +21,11 @@ function HomePage() {
                 <h1 style={{ fontSize: "24px" }}>WikiCountries: Your Guide to the World</h1>
 
                 <div className="list-group">
-                    <a className="list-group-item list-group-item-action" href="/ABW"
-                    >🇦🇼 Aruba</a>
-                    <a className="list-group-item list-group-item-action" href="/AFG"
-                    >🇦🇫 Afghanistan</a>
-                    <a className="list-group-item list-group-item-action" href="/AGO"
-                    >🇦🇴 Angola</a>
-                    <a className="list-group-item list-group-item-action" href="/AIA"
-                    >🇦🇮 Anguilla</a>
-                    <a className="list-group-item list-group-item-action" href="/ALA"
-                    >🇦🇽 Åland Islands</a>
-                    <a className="list-group-item list-group-item-action" href="/ALB"
-                    >🇦🇱 Albania</a>
-                    <a className="list-group-item list-group-item-action" href="/AND"
-                    >🇦🇩 Andorra</a>
-                    <a className="list-group-item list-group-item-action" href="/ARE"
-                    >🇦🇪 United Arab Emirates</a>
-                    <a className="list-group-item list-group-item-action" href="/ARG"
-                    >🇦🇷 Argentina</a>
-                    <a className="list-group-item list-group-item-action" href="/ARM"
-                    >🇦🇲 Armenia</a>
-                    <a className="list-group-item list-group-item-action" href="/ASM"
-                    >🇦🇸 American Samoa</a>
-                    <a className="list-group-item list-group-item-action" href="/ATA"
-                    >🇦🇶 Antarctica</a>
-                    <a className="list-group-item list-group-item-action" href="/FLK"
-                    >🇫🇰 Falkland Islands</a>
-                    <a className="list-group-item list-group-item-action active" href="/FRA"
-                    >🇫🇷 France</a>
-                    <a className="list-group-item list-group-item-action" href="/ZWE"
-                    >🇿🇼 Zimbabwe</a>
+                    {countries.map((country)=>{
+                        return <div key= {country._id} >
+                        <h3>{country.name.official}</h3>
+                        </div>
+                    })}
                 </div>
             </div>
 
