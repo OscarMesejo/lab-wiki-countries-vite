105a50b5d4ee8224d02fbe9b1bd4d434f61c741a
Oscar Mesejo <oscar.mesejo@gmail.com>
2023-08-25 16:02:10 +0200

    iteration 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import "./App.css";
+import { Routes, Route } from 'react-router-dom'
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import CountryDetails from "./pages/CountryDetailsPage";
 
 
- function App() {
+function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
       <Navbar />
-      <HomePage />
-      <CountryDetails />
-      
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/:countryId' element={<CountryDetails />} />
+      </Routes>
+
+
+
 
     </div>
 
